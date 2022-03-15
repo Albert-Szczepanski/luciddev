376027e39abe0748b9072ba7a583a2c73223c0ef
Albert <38384581+Albert-Szczepanski@users.noreply.github.com>
2022-03-15 23:57:00 +0100

    setup

---
+++ libs/photos/angular/src/lib/pages/image-resizer/image-resizer.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'image-resizer-page',
+  templateUrl: './image-resizer.page.html',
+  styleUrls: ['./image-resizer.page.scss'],
+})
+export class ImageResizerPage {
+  title = 'angular-imagerezz';
+}
--- libs/photos/angular/src/lib/photos-angular.module.ts
+++ libs/photos/angular/src/lib/photos-angular.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ImageResizerPage} from "./pages/image-resizer";
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ImageResizerPage],
+  exports: [
+    ImageResizerPage
+  ],
+  providers: []
 })
 export class PhotosAngularModule {}
